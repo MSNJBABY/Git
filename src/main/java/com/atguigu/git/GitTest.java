c29e664657a2f25bea9a2a4cd7cba85bd8fb57de
MSNJBABY <130466047+MSNJBABY@users.noreply.github.com>
2023-05-11 16:32:18 +0800

    Update GitTest.java

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,7 +8,10 @@ public class GitTest {
         System.out.println("hello git4");
         System.out.println("maste test");
         System.out.println("hot-fix test");
-        System.out.println("push test");
+        System.out.println("push test"); 
+        System.out.println("pull test");
+       
+      
 
     }
 }
